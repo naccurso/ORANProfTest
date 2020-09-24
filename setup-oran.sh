4ec293c461f3a574bfd11bd1cf6f9ed51979e826
David M. Johnson <johnsond@flux.utah.edu>
2020-09-24 13:02:26 -0600

    Upgrade e2term to version that does not decode E2SMs too.

--- setup-oran.sh
+++ setup-oran.sh
@@ -21,8 +21,21 @@ $SUDO docker login -u docker -p docker https://nexus3.o-ran-sc.org:10002
 $SUDO chown -R $SWAPPER ~/.docker
 
 #
-# Custom-build any O-RAN components we might need.
+# Custom-build the O-RAN components we might need.  Bronze release is
+# pretty much ok, but there are two components that need upgrades from
+# master:
 #
+# * e2term must not attempt to decode the E2SMs (it only supported
+#   E2SM-gNB-NRT when it was decoding them)
+# * submgr must not decode the E2SMs.
+#
+git clone https://gerrit.o-ran-sc.org/r/ric-plt/e2
+cd e2
+git checkout f13529a2ae0cae8639cfcb3504b9850a571de873
+cd RIC-E2-TERMINATION
+$SUDO docker build -f Dockerfile -t ${HEAD}.cluster.local:5000/e2term:5.4.2 .
+$SUDO docker push ${HEAD}.cluster.local:5000/e2term:5.4.2
+
 git clone https://gerrit.o-ran-sc.org/r/ric-plt/submgr
 cd submgr
 git checkout f0d95262aba5c1d3770bd173d8ce054334b8a162
@@ -40,9 +53,15 @@ helm init --client-only
 
 cp RECIPE_EXAMPLE/PLATFORM/example_recipe.yaml $OURDIR/oran
 cat <<EOF >$OURDIR/oran/example_recipe.yaml-override
+e2term:
+  alpha:
+    image:
+      registry: "${HEAD}.cluster.local:5000"
+      name: e2term
+      tag: 5.4.2
 submgr:
   image:
-    registry: "node-0.cluster.local:5000"
+    registry: "${HEAD}.cluster.local:5000"
     name: submgr
     tag: 0.5.0
 EOF
