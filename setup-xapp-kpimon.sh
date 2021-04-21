708a67606f2296b3833b8780bfa451f5677b0093
David M. Johnson <johnsond@flux.utah.edu>
2021-04-21 12:03:16 -0600

    Use correct E2 RAN name for deployed srslte config (mcc=001,mnc=001).

--- setup-xapp-kpimon.sh
+++ setup-xapp-kpimon.sh
@@ -53,7 +53,7 @@ cat <<EOF >$WWWPUB/scp-kpimon-config-file.json
             }
         }
     ],
-    "appenv": { "ranList":"enB_macro_661_8112_0019b0" },
+    "appenv": { "ranList":"enB_macro_001_001_0019b0" },
     "messaging": {
         "ports": [
             {
