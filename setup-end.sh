453fd7722def6c67cf1dc2549649efd1fefc4418
David M. Johnson <johnsond@flux.utah.edu>
2023-01-20 08:59:33 -0700

    Send more detail in final email.

--- setup-end.sh
+++ setup-end.sh
@@ -5,5 +5,14 @@ set -x
 # Grab our libs
 . "`dirname $0`/setup-lib.sh"
 
-echo "Your ${EXPTTYPE} instance setup completed on $NFQDN ." \
+(echo "Your ${EXPTTYPE} instance setup completed on $NFQDN ." ; echo ;
+ echo "Admin token for Kubernetes dashboard:" ; echo ;
+ cat /var/www/profile-private/admin-token.txt  ; echo ;
+ echo "Kubernetes namespaces:" ; echo ;
+ kubectl get namespaces ; echo ; echo ;
+ for ns in `kubectl  get namespaces --no-headers | cut -f1 -d' '` ;
+     echo "$ns" ; echo "============================================================" ; echo ;
+     kubectl -n $ns get all ;
+     echo ; echo ;
+)
     |  mail -s "${EXPTTYPE} Instance Setup Complete" ${SWAPPER_EMAIL} &
