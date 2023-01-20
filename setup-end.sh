ee2c2f3d378f5d1b063da1fc412f1950adead139
David M. Johnson <johnsond@flux.utah.edu>
2023-01-20 09:04:43 -0700

    Tweak setup end email a bit differently.

--- setup-end.sh
+++ setup-end.sh
@@ -6,13 +6,18 @@ set -x
 . "`dirname $0`/setup-lib.sh"
 
 (echo "Your ${EXPTTYPE} instance setup completed on $NFQDN ." ; echo ;
- echo "Admin token for Kubernetes dashboard:" ; echo ;
+ echo "Admin token for Kubernetes dashboard:" ; echo ; echo ; echo ;
  cat /var/www/profile-private/admin-token.txt  ; echo ;
  echo "Kubernetes namespaces:" ; echo ;
  kubectl get namespaces ; echo ; echo ;
- for ns in `kubectl  get namespaces --no-headers | cut -f1 -d' '` ;
-     echo "$ns" ; echo "============================================================" ; echo ;
+ for ns in `kubectl  get namespaces --no-headers | cut -f1 -d' '` ; do
+     echo "============================================================" ;
+     echo "$ns" ;
+     echo "============================================================" ;
+     echo ;
      kubectl -n $ns get all ;
      echo ; echo ;
-)
+ done;
+) > $OURDIR/setup-end.email ;
+cat $OURDIR/setup-end.email \
     |  mail -s "${EXPTTYPE} Instance Setup Complete" ${SWAPPER_EMAIL} &
