a5b140ef248826bfe4bdc51531b551f5f7e7bf08
David M. Johnson <johnsond@flux.utah.edu>
2023-01-23 15:41:46 -0700

    Main simulated throttle scenario script.

---
+++ demo/run-nexran-simulated-throttle.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+SLEEPINT=$1
+if [ -z "$SLEEPINT" ]; then
+    SLEEPINT=4
+fi
+
+export NEXRAN_XAPP=`kubectl get svc -n ricxapp --field-selector metadata.name=service-ricxapp-nexran-rmr -o jsonpath='{.items[0].spec.clusterIP}'`
+if [ -z "$NEXRAN_XAPP" ]; then
+    echo "ERROR: cannot find your NexRAN xApp; might need to recreate it."
+    exit 1
+fi
+
+echo NEXRAN_XAPP=$NEXRAN_XAPP ; echo
+
+
+echo Creating Simulated NodeB: ; echo
+curl -i -X POST -H "Content-type: application/json" -d '{"type":"eNB","id":411,"mcc":"001","mnc":"01"}' http://${NEXRAN_XAPP}:8000/v1/nodebs ; echo ; echo ;
+
+sleep $SLEEPINT
+
+echo Creating "'fast'" Slice: ; echo
+curl -i -X POST -H "Content-type: application/json" -d '{"name":"fast","allocation_policy":{"type":"proportional","share":1024,"auto_equalize":false,"throttle":true,"throttle_threshold":6800000,"throttle_period":1800,"throttle_share":256}}' http://${NEXRAN_XAPP}:8000/v1/slices ; echo ; echo ;
+
+sleep $SLEEPINT
+
+echo Creating "'slow'" Slice: ; echo
+curl -i -X POST -H "Content-type: application/json" -d '{"name":"slow","allocation_policy":{"type":"proportional","share":256}}' http://${NEXRAN_XAPP}:8000/v1/slices ; echo ; echo ;
+
+sleep $SLEEPINT
+
+echo Binding "'fast'" Slice to NodeB: ; echo
+curl -i -X POST http://${NEXRAN_XAPP}:8000/v1/nodebs/enB_macro_001_001_0019b0/slices/fast ; echo ; echo ;
+
+sleep $SLEEPINT
+
+echo Binding "'slow'" Slice to NodeB: ; echo
+curl -i -X POST http://${NEXRAN_XAPP}:8000/v1/nodebs/enB_macro_001_001_0019b0/slices/slow ; echo ; echo ;
+
+sleep $SLEEPINT
+
+echo Creating UE 001010123456789: ; echo
+curl -i -X POST -H "Content-type: application/json" -d '{"imsi":"001010123456789"}' http://${NEXRAN_XAPP}:8000/v1/ues ; echo ; echo ;
+
+sleep $SLEEPINT
+
+echo Binding UE "'001010123456789'" to Slice "'fast'": ; echo
+curl -i -X POST http://${NEXRAN_XAPP}:8000/v1/slices/fast/ues/001010123456789 ; echo ; echo
+
