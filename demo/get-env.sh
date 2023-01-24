24fed409b87913218fc3eb3b7cc623b9c3ef6696
David M. Johnson <johnsond@flux.utah.edu>
2023-01-24 08:37:21 -0700

    Add another env var.

--- demo/get-env.sh
+++ demo/get-env.sh
@@ -5,9 +5,11 @@ export KONG_PROXY=`kubectl get svc -n ricplt -l app.kubernetes.io/name=kong -o j
 export ONBOARDER_HTTP=`kubectl get svc -n ricplt --field-selector metadata.name=service-ricplt-xapp-onboarder-http -o jsonpath='{.items[0].spec.clusterIP}'`
 export APPMGR_HTTP=`kubectl get svc -n ricplt --field-selector metadata.name=service-ricplt-appmgr-http -o jsonpath='{.items[0].spec.clusterIP}'`
 export NEXRAN_XAPP=`kubectl get svc -n ricxapp --field-selector metadata.name=service-ricxapp-nexran-rmr -o jsonpath='{.items[0].spec.clusterIP}'`
+export INFLUXDB_IP=`kubectl get svc -n ricplt --field-selector metadata.name=ricplt-influxdb -o jsonpath='{.items[0].spec.clusterIP}'`
 
 echo E2TERM_SCTP=$E2TERM_SCTP
 echo KONG_PROXY=$KONG_PROXY
 echo ONBOARDER_HTTP=$ONBOARDER_HTTP
 echo APPMGR_HTTP=$APPMGR_HTTP
+echo INFLUXDB_IP=$INFLUXDB_IP
 echo NEXRAN_XAPP=$NEXRAN_XAPP
