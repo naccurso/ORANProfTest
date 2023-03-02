0390c193de71c5b2acbaf4d5985f3915c9396c71
David M. Johnson <johnsond@flux.utah.edu>
2023-03-02 10:37:57 -0700

    Add DBAAS_IP to env.

--- demo/get-env.sh
+++ demo/get-env.sh
@@ -6,6 +6,7 @@ export ONBOARDER_HTTP=`kubectl get svc -n ricplt --field-selector metadata.name=
 export APPMGR_HTTP=`kubectl get svc -n ricplt --field-selector metadata.name=service-ricplt-appmgr-http -o jsonpath='{.items[0].spec.clusterIP}'`
 export NEXRAN_XAPP=`kubectl get svc -n ricxapp --field-selector metadata.name=service-ricxapp-nexran-rmr -o jsonpath='{.items[0].spec.clusterIP}'`
 export INFLUXDB_IP=`kubectl get svc -n ricplt --field-selector metadata.name=ricplt-influxdb -o jsonpath='{.items[0].spec.clusterIP}'`
+export DBAAS_IP=`kubectl -n ricplt get pods/statefulset-ricplt-dbaas-server-0 -o jsonpath='{.status.podIP}'`
 
 echo E2TERM_SCTP=$E2TERM_SCTP
 echo KONG_PROXY=$KONG_PROXY
@@ -13,3 +14,4 @@ echo ONBOARDER_HTTP=$ONBOARDER_HTTP
 echo APPMGR_HTTP=$APPMGR_HTTP
 echo INFLUXDB_IP=$INFLUXDB_IP
 echo NEXRAN_XAPP=$NEXRAN_XAPP
+echo DBAAS_IP=$DBAAS_IP
