6b3510900a6224adf1c1eb5c5d32f0d2be16b0d5
David M. Johnson <johnsond@flux.utah.edu>
2023-05-22 11:16:30 -0400

    Fix srsue path.

--- demo/start-ue.sh
+++ demo/start-ue.sh
@@ -27,7 +27,7 @@ session_get $SESSION
 
 sudo ip netns add ue1
 
-tmux send-keys -t =$SESSION:0.0 'sudo srsue --rf.device_name=zmq --rf.device_args="tx_port=tcp://*:2001,rx_port=tcp://localhost:2000,id=ue,base_srate=23.04e6" --usim.algo=xor --usim.imsi=001010123456789 --usim.k=00112233445566778899aabbccddeeff --usim.imei=353490069873310 --log.all_level=warn --log.filename=stdout --gw.netns=ue1 |& tee /local/logs/srsue.log' C-m
+tmux send-keys -t =$SESSION:0.0 'sudo /local/setup/srslte-ric/build/srsue/src/srsue --rf.device_name=zmq --rf.device_args="tx_port=tcp://*:2001,rx_port=tcp://localhost:2000,id=ue,base_srate=23.04e6" --usim.algo=xor --usim.imsi=001010123456789 --usim.k=00112233445566778899aabbccddeeff --usim.imei=353490069873310 --log.all_level=warn --log.filename=stdout --gw.netns=ue1 |& tee /local/logs/srsue.log' C-m
 tmux split-window -v
 tmux send-keys -t =$SESSION:0.1 'while true ; do sleep 2 ; sudo ip netns exec ue1 ping 192.168.0.1 ; done' C-m
 #tmux select-layout even-vertical
